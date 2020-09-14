f4136d59ddfde12e0b330e572115d2aed8b2a183
hinhyuga <hinhyugau@gmail.com>
2020-09-15 07:07:39 +0800

    add return util

--- src/main/java/com/hinhyuga/watermark/utils/ReturnCode.java
+++ src/main/java/com/hinhyuga/watermark/utils/ReturnCode.java
@@ -1,5 +1,39 @@
 package com.hinhyuga.watermark.utils;
 
 public enum ReturnCode {
-    
+    SUCCESS(200,"获取成功！"),
+    FAILED(500,"服务器内部错误!");
+
+    private int key;
+    private String value;
+
+    ReturnCode(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    private static String getName(int key){
+        for (ReturnCode value : ReturnCode.values()) {
+            if (value.key == key){
+                return value.value;
+            }
+        }
+        return null;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
